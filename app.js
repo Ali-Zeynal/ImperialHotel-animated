550bd2e053e9aa79bf15026423661b97cf1f9cb6
Ali Zeynal <110941257+Ali-Zeynal@users.noreply.github.com>
2023-10-20 00:56:55 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let tdElem = document.querySelectorAll("#DoNow");
 let todayTime = new Date();
-// "October 20, 2023 17:13:00" in Date() for test
+// "October 21, 2023 11:13:00" in Date() for test
 let daytime = todayTime.getDay();
 let hourTime = todayTime.getHours();
 let minutesTime = todayTime.getMinutes();
@@ -15,158 +15,204 @@ tdArray.map(function (e) {
 function showDo() {
   if (daytime == 6) {
     if (timeNow >= 8 && timeNow < 11) {
-      doBox[0].style.backgroundColor = "red";
+      doBox[0].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[0].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 11 && timeNow < 13) {
-      doBox[1].style.backgroundColor = "red";
+      doBox[1].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[1].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 13 && timeNow < 16) {
-      doBox[2].style.backgroundColor = "red";
+      doBox[2].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[2].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 16 && timeNow < 18) {
-      doBox[3].style.backgroundColor = "red";
+      doBox[3].style.backgroundColor = "#5bbefc";
+      Array.from( doBox[3].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 18 && timeNow < 20) {
-      doBox[4].style.backgroundColor = "red";
+      doBox[4].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[4].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 20 && timeNow < 21) {
-      doBox[5].style.backgroundColor = "red";
+      doBox[5].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[5].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 21 && timeNow < 22) {
-      doBox[6].style.backgroundColor = "red";
+      doBox[6].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[6].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 22 && timeNow < 23) {
-      doBox[7].style.backgroundColor = "red";
+      doBox[7].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[7].children)[0].children[0].style.color = "#010072";
     }
   }
   if (daytime == 0) {
     if (timeNow >= 8 && timeNow < 10) {
-      doBox[0].style.backgroundColor = "red";
+      doBox[0].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[0].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 10 && timeNow < 12) {
-      doBox[1].style.backgroundColor = "red";
+      doBox[1].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[1].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 12 && timeNow < 15) {
-      doBox[2].style.backgroundColor = "red";
+      doBox[2].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[2].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 15 && timeNow < 17) {
-      doBox[3].style.backgroundColor = "red";
+      doBox[3].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[3].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 17 && timeNow < 19) {
-      doBox[4].style.backgroundColor = "red";
+      doBox[4].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[4].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 19 && timeNow < 21) {
-      doBox[5].style.backgroundColor = "red";
+      doBox[5].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[5].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 21 && timeNow < 22) {
-      doBox[6].style.backgroundColor = "red";
+      doBox[6].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[6].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 22 && timeNow < 23) {
-      doBox[7].style.backgroundColor = "red";
+      doBox[7].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[7].children)[0].children[0].style.color = "#010072";
     }
   }
   if (daytime == 1) {
     if (timeNow >= 8 && timeNow < 9) {
-      doBox[0].style.backgroundColor = "red";
+      doBox[0].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[0].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 9 && timeNow < 10) {
-      doBox[1].style.backgroundColor = "red";
+      doBox[1].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[1].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 10 && timeNow < 12) {
-      doBox[2].style.backgroundColor = "red";
+      doBox[2].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[2].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 12 && timeNow < 16) {
-      doBox[3].style.backgroundColor = "red";
+      doBox[3].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[3].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 16 && timeNow < 17) {
-      doBox[4].style.backgroundColor = "red";
+      doBox[4].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[4].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 17 && timeNow < 19) {
-      doBox[5].style.backgroundColor = "red";
+      doBox[5].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[5].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 19 && timeNow < 22) {
-      doBox[6].style.backgroundColor = "red";
+      doBox[6].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[6].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 22 && timeNow < 23) {
-      doBox[7].style.backgroundColor = "red";
+      doBox[7].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[7].children)[0].children[0].style.color = "#010072";
     }
   }
   if (daytime == 2) {
     if (timeNow >= 10 && timeNow < 12) {
-      doBox[0].style.backgroundColor = "red";
+      doBox[0].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[0].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 12 && timeNow < 14) {
-      doBox[1].style.backgroundColor = "red";
+      doBox[1].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[1].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 14 && timeNow < 15) {
-      doBox[2].style.backgroundColor = "red";
+      doBox[2].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[2].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 15 && timeNow < 18) {
-      doBox[3].style.backgroundColor = "red";
+      doBox[3].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[3].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 18 && timeNow < 21) {
-      doBox[4].style.backgroundColor = "red";
+      doBox[4].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[4].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 21 && timeNow < 22) {
-      doBox[5].style.backgroundColor = "red";
+      doBox[5].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[5].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 22 && timeNow < 23) {
-      doBox[6].style.backgroundColor = "red";
+      doBox[6].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[6].children)[0].children[0].style.color = "#010072";
     }
   }
   if (daytime == 3) {
     if (timeNow >= 8 && timeNow < 10) {
-      doBox[0].style.backgroundColor = "red";
+      doBox[0].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[0].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 10 && timeNow < 12) {
-      doBox[1].style.backgroundColor = "red";
+      doBox[1].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[1].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 12 && timeNow < 15) {
-      doBox[2].style.backgroundColor = "red";
+      doBox[2].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[2].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 15 && timeNow < 17) {
-      doBox[3].style.backgroundColor = "red";
+      doBox[3].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[3].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 17 && timeNow < 19) {
-      doBox[4].style.backgroundColor = "red";
+      doBox[4].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[4].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 19 && timeNow < 22) {
-      doBox[5].style.backgroundColor = "red";
+      doBox[5].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[5].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 22 && timeNow < 23) {
-      doBox[6].style.backgroundColor = "red";
+      doBox[6].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[6].children)[0].children[0].style.color = "#010072";
     }
   }
   if (daytime == 4) {
     if (timeNow >= 10 && timeNow < 12) {
-      doBox[0].style.backgroundColor = "red";
+      doBox[0].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[0].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 12 && timeNow < 14) {
-      doBox[1].style.backgroundColor = "red";
+      doBox[1].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[1].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 14 && timeNow < 15) {
-      doBox[2].style.backgroundColor = "red";
+      doBox[2].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[2].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 15 && timeNow < 18) {
-      doBox[3].style.backgroundColor = "red";
+      doBox[3].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[3].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 18 && timeNow < 21) {
-      doBox[4].style.backgroundColor = "red";
+      doBox[4].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[4].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 21 && timeNow < 22) {
-      doBox[5].style.backgroundColor = "red";
+      doBox[5].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[5].children)[0].children[0].style.color = "#010072";
     }
     if (timeNow >= 22 && timeNow < 23) {
-      doBox[6].style.backgroundColor = "red";
+      doBox[6].style.backgroundColor = "#5bbefc";
+      Array.from(doBox[6].children)[0].children[0].style.color = "#010072";
     }
   }
   if (daytime == 5) {
-    doBox[0].style.backgroundColor = "red";
+    doBox[0].style.backgroundColor = "#5bbefc";
+    Array.from(doBox[0].children)[0].children[0].style.color = "#010072";
   }
-  if (timeNow >= 23) {
+  if (timeNow >= 23 || timeNow < 8) {
     let div = document.querySelector("#table");
     div.innerHTML =
-      "<h1 style='background-color:red; margin-top: 300px; font-size:60px;'>پایان زمان بندی</h1>";
+      "<h1 style='background-color: #5bbefc; margin-top: 300px; font-size: 100px; color : #010072'>پایان زمان بندی</h1>";
   }
 }
 window.addEventListener("load", function () {
